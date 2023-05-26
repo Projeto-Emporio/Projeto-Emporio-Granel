686dacd98c4c8dc563019df1310ff69bdca36117
Projeto-Emporio <emporioprojeto@gmail.com>
2023-05-26 19:56:02 -0300

    tirando os script que nao e necessario

--- script/script.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector('.container');
-	const sidebar = document.querySelector('.sidebar');
-	const closeBtn = document.querySelector('.close-btn');
-    const carrinhoIcone = document.querySelector('.carrinho-icone');
-	closeBtn.addEventListener('click', () => {
-	    container.classList.remove('active');
-	});
-	
